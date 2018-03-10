4b7fbf00baa2cdf9bb28bac54105fa669d40e744
Aleseea <pen14002@byui.edu>
2018-03-10 16:09:22 -0700

    uploading my shipping cost calculator

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
+var calculate = require('./calculate.js');
+
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
+  .get('/calculate', calculate.calculate)
+  /*.get('/getRate', (req, res) => res.render('pages/index'))*/
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
